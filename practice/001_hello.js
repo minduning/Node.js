364860aab93d74368b20b2628be9fca4ee4e5a64
minduning <conan20700@gmail.com>
2025-09-16 23:13:22 +0900

    Create 001_hello.js

---
+++ practice/001_hello.js
@@ -0,0 +1,8 @@
+console.log('hello');
+console.log('hello', 'hi');
+console.warn(`this line ${'can make error'}`);
+
+// Node.js의 출력은 console.log()를 이용한다.
+// console.log()는 디버깅을 하거나 로그를 남길 때 사용한다.
+// 여러 개를 출력하고 싶다면 콤마를 사용하면 된다.
+// `(백틱)으로 감싸주면 템플릿처럼 사용할 수 있다.
